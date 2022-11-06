3a20312ec4c54349bf7c17188777ef3c178447cc
Darkfioufiou <47570065+Darkfioufiou@users.noreply.github.com>
2022-11-06 14:09:04 +0100

    Anim tracteur + Fix des spawnActor

--- Assets/EndlessRunner/Script/KillZone.cs
+++ Assets/EndlessRunner/Script/KillZone.cs
@@ -9,17 +9,21 @@ public class KillZone : MonoBehaviour
     public GameObject Parent;
     public GameObject SpawnZone_01;
     public GameObject SpawnZone_02;
+    public int numProps;
     private void OnTriggerEnter(Collider other)
     {
 
         if (other.gameObject.tag == "ground")
         {
             Debug.Log("<color=yellow>hit</color>");
-           var myNewGround = Instantiate(Ground, originSpawn.transform.position, Quaternion.Euler(0,90,0));
+            var myNewGround = Instantiate(Ground, originSpawn.transform.position, Quaternion.Euler(0, 90, 0));
 
             myNewGround.transform.parent = Parent.transform;
-            SpawnZone_01.GetComponent<SpawnRandomActor>().SpawnFood();
-            SpawnZone_02.GetComponent<SpawnRandomActor>().SpawnFood();
+            for (int i = 0; i < numProps; i++)
+            {
+                SpawnZone_01.GetComponent<SpawnRandomActor>().SpawnFood();
+                SpawnZone_02.GetComponent<SpawnRandomActor>().SpawnFood();
+            }
         }
     }
 }
